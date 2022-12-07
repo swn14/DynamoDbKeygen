f6003a14dc75485c55abca84eb13afd457ed0586
Sheldon Nofer <snofer@bethel.jw.org>
2022-12-06 23:22:20 -0500

    WIP: add initial form for generator

---
+++ src/DynamoDbKeygen.Application/DynamoDbKeyModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DynamoDbKeygen.Application;
+
+public class DynamoDbKeyModel
+{
+    public string RootNamespace { get; set; }
+    public string RootIdentifier { get; set; }
+    public string DomainNamespace { get; set; }
+    public string DomainIdentifier { get; set; }
+}
--- src/DynamoDbKeygen.Application/Pages/Index.cshtml.cs
+++ src/DynamoDbKeygen.Application/Pages/Index.cshtml.cs
@@ -6,14 +6,24 @@ namespace DynamoDbKeygen.Application.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    public DynamoDbKeyModel ViewModel;
 
     public IndexModel(ILogger<IndexModel> logger)
     {
         _logger = logger;
+        ViewModel = new DynamoDbKeyModel();
     }
 
     public void OnGet()
     {
 
+    }
+    
+    public void OnPost()
+    {
+        ViewModel.RootNamespace = Request.Form["rootNamespace"];
+        ViewModel.RootIdentifier = Request.Form["rootIdentifier"];
+        ViewModel.DomainNamespace = Request.Form["domainNamespace"];
+        ViewModel.DomainIdentifier = Request.Form["domainIdentifier"];
     }
 }
